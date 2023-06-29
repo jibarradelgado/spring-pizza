642e4ba96b209f3ad633263683240f6461d579a5
Jorge Ibarra <hello@jorgeibarra.com.mx>
2023-06-29 15:15:11 -0600

    First commit'

---
+++ src/main/java/com/jibarrad/pizza/JibarradPizzeriaApplication.java
@@ -0,0 +1,13 @@
+package com.jibarrad.pizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JibarradPizzeriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JibarradPizzeriaApplication.class, args);
+	}
+
+}
