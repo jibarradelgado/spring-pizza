54efed5c573d376383f1a626a702059948d5c65e
Jorge Ibarra <hello@jorgeibarra.com.mx>
2023-06-30 11:30:42 -0600

    Added security configuration

--- src/main/java/com/jibarrad/pizza/web/config/SecurityConfig.java
+++ src/main/java/com/jibarrad/pizza/web/config/SecurityConfig.java
@@ -2,9 +2,12 @@ package com.jibarrad.pizza.web.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.server.util.matcher.PathPatternParserServerWebExchangeMatcher;
 
 @Configuration
 public class SecurityConfig {
@@ -12,9 +15,14 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(customizeRequests -> {
             customizeRequests
+                    .requestMatchers(HttpMethod.GET,"/api/**").permitAll()
+                    .requestMatchers(HttpMethod.PUT).denyAll()
                     .anyRequest()
                     .authenticated();
-        }).httpBasic(Customizer.withDefaults());
+        })
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults());
         return http.build();
     }
 }
